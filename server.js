44fb312af4c1cc38e05d0126448db36b76b39995
Waele, Antonie de (ITCDCC) - KLM <Antonie-de.Waele@klm.com>
2019-01-03 13:31:09 +0100

    Remove that rotzooi

--- server.js
+++ server.js
@@ -12,5 +12,8 @@ app.use(express.static(path.resolve(__dirname, "dist")));
 app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
-
+//kan jij mij mlab even snel laten zien?
+// doet hij nou iets?
+//username: anton
+//password: wachtwoord123
 app.listen(process.env.PORT || 8010);
